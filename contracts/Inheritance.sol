ca5d3b49fd63da2c062cabc33e050dcc1b673c3c
khadgachy07 <khadgachy07@gmail.com>
2022-04-17 14:26:59 +0545

    Inheritance

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENCED
+
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+}
+
+contract SecretVault {
+    string secret;
+    
+    constructor(string memory _secret) public {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory) {
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
+    address secretVault;
+
+    constructor(string memory _secret) public {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns(string memory) {
+        SecretVault _secretVault = SecretVault(secretVault);
+        return _secretVault.getSecret();
+    }
+}
